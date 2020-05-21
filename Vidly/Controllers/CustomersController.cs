c7e346348d6bc188195185288bbfd54b30b30098
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-22 00:38:22 +0200

    Edit Customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -78,6 +78,22 @@ namespace Vidly.Controllers
 
             return View(customer);
         }
+        
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
 
     }
 }
