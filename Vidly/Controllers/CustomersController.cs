b03605c6942d2b007a702e41060dae42bbc7eaf4
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-21 16:16:22 +0200

    Fetched the data from DB

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        
+        public ViewResult Index()
+        {
+            return View(GetCustomers());
+        }
+        
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+        
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+    }
+}
