26d017561435fb28616df05326d52f62a9ba7f4d
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 12:39:26 +0200

    Included SQL script to generate database on any machine

---
+++ Vidly/Migrations/202006011024310_RunSqlScript.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class RunSqlScript : DbMigration
+    {
+        public override void Up()
+        {
+            var sqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../SqlScripts/01.06.2020_sqlScript.sql");
+            Sql(File.ReadAllText(sqlFile));
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
